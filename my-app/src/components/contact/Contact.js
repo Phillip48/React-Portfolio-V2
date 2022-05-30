81f6d177945db59643635a5f3bb3cbced57e79a4
Phillip48 <phillippereira02@gmail.com>
2022-05-29 20:40:02 -0400

    Added contact to js and css. Made small updates to navbar

---
+++ my-app/src/components/contact/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Contact.css';
+// Npm installed icons. just need to use
+// import { BiWorld } from "react-icons/bi";
+
+const Contact = () => {
+    return (
+        <section>
+
+        </section>
+    )
+}
+
+export default Contact;
