081a872a451c64be546022261dbc1d2a0f710d86
Phillip48 <phillippereira02@gmail.com>
2022-06-01 00:09:08 -0400

    Changed some hover effects and add a hover effect in technologies

--- my-app/src/components/technologies/Technologies.js
+++ my-app/src/components/technologies/Technologies.js
@@ -9,22 +9,36 @@ const Technologies = () => {
             <p className='para-text-small margin-side'>I've worked with...</p>
             <div className='div-padding'></div>
             <div className='technologies-worked-display'>
-                <div>
+                <div className='tech-experience'>
                     <h3 className='sub-intro-med'>Front-End</h3>
                     <p className='para-text-small'>Experience with...</p>
-                    <p className='para-text-small'>HTML</p>
+                    <p className='para-text-small'>HTML5</p>
+                    <p className='para-text-small'>CSS3</p>
+                    <p className='para-text-small'>Bootstrap</p>
+                    <p className='para-text-small'>Bulma</p>
+                    <p className='para-text-small'>Tailwind</p>
+                    <p className='para-text-small'>React</p>
                 </div>
 
-                <div>
+                <div className='tech-experience'>
                     <h3 className='sub-intro-med'>Back-End</h3>
                     <p className='para-text-small'>Experience with...</p>
                     <p className='para-text-small'>JavaScript</p>
+                    <p className='para-text-small'>Node JS</p>
+                    <p className='para-text-small'>Express JS</p>
+                    <p className='para-text-small'>MongoDb</p>
+                    <p className='para-text-small'>Mongoose</p>
+                    <p className='para-text-small'>GraphQl</p>
                 </div>
 
-                <div>
-                    <h3 className='sub-intro-med'>UI/UX</h3>
-                    <p className='para-text-small'>Experience with...</p>
-                    <p className='para-text-small'>Tools like figma</p>
+                <div className='tech-experience'>
+                    <h3 className='sub-intro-med'>Dev Tools</h3>
+                    <p className='para-text-small'>Such as...</p>
+                    <p className='para-text-small'>Figma</p>
+                    <p className='para-text-small'>Github</p>
+                    <p className='para-text-small'>Postman</p>
+                    <p className='para-text-small'>Heroku</p>
+                    <p className='para-text-small'>Insomnia</p>
                 </div>
             </div>
 
