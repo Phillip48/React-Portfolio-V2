30bd580980bbffb68ea2e3b9f80dace50cb6b609
Phillip48 <phillippereira02@gmail.com>
2022-05-31 23:28:03 -0400

    Added a type writer to the about me

--- my-app/src/components/about me/AboutMe.js
+++ my-app/src/components/about me/AboutMe.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './AboutMe.css';
+import Type from '../typing/Type';
 // import peronsalLogo from '../image/Myproject.png';
 import peronsalLogo2 from '/Users/phill/OneDrive/Documents/Peronsal Project coding/React-Portfolio-V2/my-app/src/image/1P-logos_transparent.png';
 // import BgAnimation from '../components/BackgroundAnimation';
@@ -11,6 +12,10 @@ const Navbar = () => {
                 <section className='holds-both-sides'>
                     <section className='leftside-text'>
                         <h1 className='intro-big'>Hi! I'm <span className='underline-name'>Phillip!</span></h1>
+                        <div className='type-words'>
+                            <Type />
+                        </div>
+                        <div className='div-padding-aboutme'></div>
                         <h3 className='sub-intro-med'>Welcome to my portfolio!</h3>
                         <p className='para-text-small'>I'm a fullstack developer with a passion for web design and making apps.
                             I enjoy developing various websites and apps that provide real value to the end user.
