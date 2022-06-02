d695567b439f7d7438980c3c6595f5d78715b939
Phillip48 <phillippereira02@gmail.com>
2022-06-01 23:15:08 -0400

    Responsive design for technologies, contact and footer

--- my-app/src/components/footer/Footer.js
+++ my-app/src/components/footer/Footer.js
@@ -7,12 +7,12 @@ const Footer = () => {
         <footer className='margin-side-footer '>
             <div className='linebreak-footer'></div>
             <section className='footer-flex'>
-                <div className=''>
+                <div className='footer-responsive'>
                     <h3 className='sub-intro-med-footer'>Call:</h3>
                     <a href='tel:3219468987'><p className='para-text-small-footer'>321-946-8987</p></a>
                 </div>
-                <div className='div-padding'></div>
-                <div className=''>
+                <div className='div-padding '></div>
+                <div className='footer-responsive'>
                     <h3 className='sub-intro-med-footer'>Email:</h3>
                     <a href='mailto: phillippereira02@gmail.com'><p className='para-text-small-footer'>phillippereira02@gmail.com</p></a>
                 </div>
