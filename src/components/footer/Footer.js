cbba49e7724e97271b4d90890cab482b86ddde9f
Phillip48 <phillippereira02@gmail.com>
2022-06-02 15:51:37 -0400

    Update styling to look better on phone

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -9,12 +9,12 @@ const Footer = () => {
             <section className='footer-flex'>
                 <div className='footer-responsive'>
                     <h3 className='sub-intro-med-footer'>Call:</h3>
-                    <a href='tel:3219468987'><p className='para-text-small-footer'>321-946-8987</p></a>
+                    <a href='tel:3219468987'><p className='para-text-small-footer'> 321-946-8987</p></a>
                 </div>
                 <div className='div-padding '></div>
                 <div className='footer-responsive'>
                     <h3 className='sub-intro-med-footer'>Email:</h3>
-                    <a href='mailto: phillippereira02@gmail.com'><p className='para-text-small-footer'>phillippereira02@gmail.com</p></a>
+                    <a href='mailto: phillippereira02@gmail.com'><p className='para-text-small-footer'> phillippereira02@gmail.com</p></a>
                 </div>
                 <div>
                     <a href='https://github.com/Phillip48'><AiFillGithub className='footer-icons' /></a>
