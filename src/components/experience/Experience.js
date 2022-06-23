bb05b16f4fbc4426c06adf9759f803b231ca91e9
Phillip48 <phillippereira02@gmail.com>
2022-06-23 08:28:45 -0400

    Added resume download in navbar and a commented out experience section

--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -15,30 +15,35 @@ const Experience = () => {
                 <section className='section-left-experience'>
                     <div className=' one-project margin-top'>
                         <a href='https://github.com/Phillip48/country-V2'><img src={travelGuide} alt='project-img' className='experience-img'></img>
-                        <h3 className='sub-intro-med-experience'>🌎 Tarvelers Guide <button className='linktosite'>Check it out!</button></h3> 
-                        <p className='para-text-small margin-side'>Who doesn't want to travel the world? This app allows you to see where a country is located and some information about the country. Not to mention follow other users and review the countries youve visited... and of course have a favorites list.</p>
+                            <h3 className='sub-intro-med-experience'>🌎 Tarvelers Guide <button className='linktosite'>Check it out!</button></h3>
+                            <p className='para-text-small margin-side'>Who doesn't want to travel the world? This app allows you to see where a country is located and some information about the country. Not to mention follow other users and review the countries youve visited... and of course have a favorites list.</p>
                         </a>
                     </div>
                     {/* <div></div> */}
                     <div className=' one-project margin-top'>
                         <a href='https://github.com/Phillip48/reel-good-movies'><img src={moviesPic} alt='project-img' className='experience-img'></img>
-                        <h3 className='sub-intro-med-experience'>🍿 Reel Good Movies <button className='linktosite'>Check it out!</button></h3>
-                        <p className='para-text-small margin-side'>Reel Good Movies is a full stack web application where a user can register an account, search in a movie database, post a review for movie, and save movies to their watched and will watch profile lists.</p>
+                            <h3 className='sub-intro-med-experience'>🍿 Reel Good Movies <button className='linktosite'>Check it out!</button></h3>
+                            <p className='para-text-small margin-side'>Reel Good Movies is a full stack web application where a user can register an account, search in a movie database, post a review for movie, and save movies to their watched and will watch profile lists.</p>
                         </a>
                     </div>
                 </section>
                 <section className='section-right-experience'>
                     <div className=' one-project margin-top'>
                         <a href="https://github.com/Phillip48/route-my-day"><img src={routeMyDay} alt='project-img' className='experience-img'></img>
-                        <h3 className='sub-intro-med-experience'>🛣️ Route My Day <button className='linktosite'>Check it out!</button></h3>
-                        <p className='para-text-small margin-side'>Route my day is a website that provides travelers with a service to schedule their travel routes for daily trips. This web app works by finding local business in a city of your choice and then adds them to a jQuery list to sort. Then it takes that list and makes your route using google maps.</p>
+                            <h3 className='sub-intro-med-experience'>🛣️ Route My Day <button className='linktosite'>Check it out!</button></h3>
+                            <p className='para-text-small margin-side'>Route my day is a website that provides travelers with a service to schedule their travel routes for daily trips. This web app works by finding local business in a city of your choice and then adds them to a jQuery list to sort. Then it takes that list and makes your route using google maps.</p>
                         </a>
                     </div>
                 </section>
-
-
+                {/* <section className='section-right-experience'>
+                    <div className=' one-project margin-top'>
+                        <a href="https://github.com/Phillip48/route-my-day"><img src={moviesPic} alt='project-img' className='experience-img'></img>
+                            <h3 className='sub-intro-med-experience'> <button className='linktosite'>Check it out!</button></h3>
+                            <p className='para-text-small margin-side'></p>
+                        </a>
+                    </div>
+                </section> */}
             </div>
-
         </section>
     )
 }
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './Navbar.css';
 // import peronsalLogo from '../image/Myproject.png';
 import peronsalLogo from '/Users/phill/OneDrive/Documents/Peronsal Project coding/React-Portfolio-V2/src/image/1Phillip Pereira-logos_transparent.png';
+// import resume from '"C:\Users\phill\OneDrive\Documents\Peronsal Project coding\resume folder\Updated Resume- Phillip Pereira (1).pdf"';
+
 
 const Navbar = () => {
     return (
@@ -15,6 +17,7 @@ const Navbar = () => {
                 <a href='#aboutme'><h4 className='element-in-navbar'>About Me</h4></a>
                 <a href='#experience'><h4 className='element-in-navbar'>Experience</h4></a>
                 <a href='#contact'><h4 className='element-in-navbar'>Contact Me</h4></a>
+                <a href='https://docs.google.com/document/d/1IOyJKIY2Gops2HWI2NI0Ob0huubyN70pMmlAs0u-FX8/export?format=pdf'><h4 className='element-in-navbar'>Resume</h4></a>
             </navbar>
         </section>
         {/*  To use with whatever part of the page the user is on. It will get highlighted in the navbar */}
